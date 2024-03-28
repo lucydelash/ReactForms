e70bab8800fb3b3117988692b938fadcd70537a9
lucydelash <lucyrudd614@gmail.com>
2024-03-28 16:41:20 -0400

    pushing completed workshop

--- PizzahPizzah/src/App.jsx
+++ PizzahPizzah/src/App.jsx
@@ -1,48 +1,57 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [newOption, setNewOption] = useState('')
-  const [pizzaOptions, setPizzaOptions] = useState(["Margherita", "Pepperoni", "Vegetarian", "BBQ", "Supreme"]);
+  const [newOption, setNewOption] = useState('');
+  const [newTopping, setNewTopping] = useState('');
+  const [pizzaOptions, setPizzaOptions] = useState(["Margherita", "Pepperoni", "Vegetarian", "BBQ Chicken", "Supreme"]);
+  const [pizzaToppings, setPizzaToppings] = useState(["Cheese & Basil", "Do I Really Need To Say...", "ALL THE VEGGIES!", "Chicken & BBQ Sauce", "EVERYTHING!!!"]);
 
-  const handleChange = (e) => {
+  const handleOptionChange = (e) => {
     setNewOption(e.target.value);
+  }
+
+  const handleToppingChange = (e) => {
+    setNewTopping(e.target.value);
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (newOption.trim() !== '') {
+    if (newOption.trim() !== '' && newTopping.trim() !== '') {
       setPizzaOptions([...pizzaOptions, newOption]);
+      setPizzaToppings([...pizzaToppings, newTopping]);
       setNewOption('');
+      setNewTopping('');
     }
-  }
-
-  const handleClick = () => {
-    // Code to handle button click goes here
   }
 
   return (
     <>
-      <h1>Pizzah Pizzah</h1>
+      <img src="https://github.com/lucydelash/PizzahPizzah/blob/main/5b8849a8-4436-433b-bdfe-d09b26cd0a89.png?raw=true"></img>
 
-      {/* Rendering pizza options */}
-      <ul>
-        {pizzaOptions.map((pizza, index) => (
-          <li key={index}>{pizza}</li>
-        ))}
-      </ul>
-
-      {/* Form to add new pizza option */}
+      {/* Combined Form: Add new pizza option and topping */}
       <form onSubmit={handleSubmit}>
         <label>
-          Create Your Own Pizzah:
-          <input type="text" value={newOption} onChange={handleChange} />
+          Name Your Pizzah:
+          <input type="text" value={newOption} onChange={handleOptionChange} />
         </label>
-        <button className="button5" onClick={handleClick}>Pizzah?</button>
+        <label>
+          Add Your Toppings:
+          <input type="text" value={newTopping} onChange={handleToppingChange} />
+        </label>
+        <button className="button5" type="submit">Pizzah!</button>
       </form>
+
+      {/* Rendering pizza options and toppings */}
+      <div>
+        <ul>
+          {pizzaOptions.map((option, index) => (
+            <li key={index}>{option} - {pizzaToppings[index]}</li>
+          ))}
+        </ul>
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
